4490c008f1af319a13a6e859ac495ed1df6a2827
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-19 11:53:12 -0800

    User controller/create

--- project_vagabond/app/controllers/users_controller.rb
+++ project_vagabond/app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
 
-	
+	def new
+		
+	end
+
+	def create
+		user = User.new(user_params)
+		if user.save
+			session[:user_id] = user.id
+			redirect_to '/'
+		else
+			redirect_to '/signup'
+		end
+	end
+
+private
+
+	def user_params
+		params.require(:user).permit(:first_name, :email, :last_name, :password, :password_confirmation, :current_city)
+	end
 	
 end
