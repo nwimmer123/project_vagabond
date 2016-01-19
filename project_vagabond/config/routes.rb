09acd0ffe2d34b283c713db2de469efb42edae89
jroers <joe.roers@gmail.com>
2016-01-19 12:26:12 -0800

    Added users#index method and view. Added root to: in routes

--- project_vagabond/config/routes.rb
+++ project_vagabond/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+	root to: "users#index"
 
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+	get '/home', to: 'users#create'
+  get '/signup', to: 'users#new', as: "new_user"
+  post '/users', to: 'users#create'
 
 end
