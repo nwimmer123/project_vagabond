9e176046447ffcf01b51f9e36c4411aad9a1b068
jroers <joe.roers@gmail.com>
2016-01-20 11:25:05 -0800

    Added edit/update method to entries and an edit link on the entry show page

--- project_vagabond/app/controllers/entries_controller.rb
+++ project_vagabond/app/controllers/entries_controller.rb
@@ -7,4 +7,16 @@ class EntriesController < ApplicationController
 		@entry = Entry.find_by_id(params[:id])
 		@user = User.find_by_id(@entry.user_id)
 	end
+
+	def edit
+		@entry = Entry.find_by_id(params[:id])
+	end
+
+	def update
+		entry_params = params.require(:entry).permit(:title, :body)
+		@entry = Entry.find_by_id(params[:id])
+		@entry.update_attributes(entry_params)
+
+		redirect_to entry_path
+	end
 end
