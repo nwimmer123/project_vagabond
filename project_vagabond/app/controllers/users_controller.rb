b75c2104bbb4f61b0d016a7f1b596fe5a57b24c9
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-21 12:20:52 -0800

    refactored to use login helper in user create method

--- project_vagabond/app/controllers/users_controller.rb
+++ project_vagabond/app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 	def create
 		user = User.new(user_params)
 		if user.save
-			session[:user_id] = user.id
+			login(user)
 			redirect_to my_profile_path
 		else
 			flash[:error] = "Email has already been taken"
